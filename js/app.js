73cd2b9d52ff3a392d83fc78381a1c693c499342
Todd Tate <toddtatevw@gmail.com>
2019-03-18 11:19:34 -0600

    Inital File Upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDoDfAUbIIVeMl3gbfwW5c1JGa8foDsQy4",
+    authDomain: "cheap-date-database.firebaseapp.com",
+    databaseURL: "https://cheap-date-database.firebaseio.com",
+    projectId: "cheap-date-database",
+    storageBucket: "cheap-date-database.appspot.com",
+    messagingSenderId: "715258876899"
+  };
+  firebase.initializeApp(config);
