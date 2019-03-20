748e6a8b1f018e37ae9e8609a94912268c3251cd
sturdy75 <sturdy75@gmail.com>
2019-03-20 11:26:55 -0600

    Wednesday update

--- js/cheap.js
+++ js/cheap.js
@@ -1,62 +1,81 @@
-//show
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDoDfAUbIIVeMl3gbfwW5c1JGa8foDsQy4",
-    authDomain: "cheap-date-database.firebaseapp.com",
-    databaseURL: "https://cheap-date-database.firebaseio.com",
-    projectId: "cheap-date-database",
-    storageBucket: "cheap-date-database.appspot.com",
-    messagingSenderId: "715258876899"
-  };
-  firebase.initializeApp(config);
-
-var dataRef = firebase.database();
-
-var ingredient = "";
-var tvshow = "";
-
-//Capture Button Click
-$("#add-ingredient").on("click", function(event){
-event.preventDefault();
-
-ingredient = $("#ingredient-input").val().trim();
-tvshow = $("tvshow-input").val().trim();
-
-dataRef.ref().push({
-  ingredient: ingredient,
-    tvshow: tvshow,
-
-});
-});
-
-dataRef.ref().on("child_added", function(childSnapshot) {
-
-console.log(childSnapshot.val().ingredient);
-console.log(childSnapshot.val().tvshow);
+// // Capture Time Button Click
+$("#add-time").on("click", function (event) {
+  event.preventDefault();
+  var tvQueryURL = "http://api.tvmaze.com/schedule?country=US&date=2019-03-24";
+  var time = $("#add-time").val().trim();
 
 
-
-
-})
-
-
-var showQueryURL = "http://api.tvmaze.com/schedule?country=US&date=2019-03-18";
-
-$.ajax({
-    url: showQueryURL,
+  $.ajax({
+    url: tvQueryURL,
     method: "GET"
-}).then(function (response) {
-    console.log(response.airdate);
+  }).then(function (response) {
+    var tvResults = response;
+    console.log(tvResults);
 
-})
+    for (var i = 0; i < tvResults.length; i++) {
+      var showImage = tvResults[i].show.image.original;
+      var showName = tvResults[i].show.name;
+      var showURL = tvResults[i].show.url;
+      var showTime = tvResults[i].show.airtime;
 
-.then(function(response) {
-
-    console.log(showQueryURL);
 
 
+        $(".tvimg" + (i + 1)).attr("src", showImage);
+        $(".tvimg" + (i + 1)).attr("url", showURL);
+        $(".tvbox" + (i + 1)).prepend($(".showName").text(showName));
+        $(".showName" + (i + 1)).text(showName);
+  
+        $("img").on("click", function (event) {
+          var url = $(this).attr("url");
+          console.log(url);
+          window.location = url;
+        })
+        
+      }
 
-})
+     
     
+  })
+})
+
+
+
+  //Capture  Food Button Click
+  $("#add-ingredient").on("click", function (event) {
+    event.preventDefault();
+    var ingredient = $("#ingredient").val().trim();
+
+    console.log(ingredient);
+    var ingredientQueryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=9c1f98e9&app_key=aaea5f847e32e06cf25133ccb9fdb4c0%20";
+
+
+    $.ajax({
+      url: ingredientQueryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      var results = response.hits;
+      console.log(results);
+
+      for (var i = 0; i < results.length; i++) {
+        var image = results[i].recipe.image;
+        var recName = results[i].recipe.label;
+        var recURL = results[i].recipe.shareAs;
+
+        // var html = 
+        $(".img" + (i + 1)).attr("src", image);
+        $(".img" + (i + 1)).attr("url", recURL);
+        $(".box" + (i + 1)).prepend($(".recipeName").text(recName));
+        $(".recipeName" + (i + 1)).text(recName);
+
+        $("img").on("click", function (event) {
+          var url = $(this).attr("url");
+          console.log(url);
+          window.location = url;
+        })
+      }
+    })
+  })
+
 
 
